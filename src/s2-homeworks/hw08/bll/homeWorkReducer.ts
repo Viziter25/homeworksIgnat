7e88782f776c48ffc0ecca77735e646895bca91b
Zlotnik-Ilya <ilya.zlotnik@mail.ru>
2022-11-02 18:09:08 +0300

    feat: add hw08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,54 @@
 import {UserType} from '../HW8'
 
+
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+// type ActionType = upSortACType
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload === 'up') {
+                return  [...state.sort((a, b) => {
+                    if (a.name > b.name) {
+                        return 1
+                    } else if (a.name < b.name) {
+                        return -1
+                    } else {
+                        return 0
+                    }
+                })]
+            }
+            if (action.payload === 'down') {
+                return  [...state.sort((a, b) => {
+                    if (a.name < b.name) {
+                        return 1
+                    } else if (a.name > b.name) {
+                        return -1
+                    } else {
+                        return 0
+                    }
+                })]
+            }
+            else return state
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload )
         }
         default:
             return state
     }
 }
+
+// type upSortACType = ReturnType<typeof upSortAC>
+// export const upSortAC = (up:string) => {
+//     return {
+//         type: 'sort',
+//         payload: {
+//             up:up
+//         }
+//     }as const
+// }
