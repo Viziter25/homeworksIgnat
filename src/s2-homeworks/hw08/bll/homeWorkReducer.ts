f509891d00db5e9d38757cb90b418bd570148682
Zlotnik-Ilya <ilya.zlotnik@mail.ru>
2022-11-02 18:38:38 +0300

    feat: add hw08 (corrected error)

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -23,10 +23,10 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
             }
             if (action.payload === 'down') {
                 return  [...state.sort((a, b) => {
-                    if (a.name < b.name) {
-                        return 1
-                    } else if (a.name > b.name) {
+                    if (a.name > b.name) {
                         return -1
+                    } else if (a.name < b.name) {
+                        return 1
                     } else {
                         return 0
                     }
