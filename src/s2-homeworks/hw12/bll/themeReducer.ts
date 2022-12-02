c3c73e2e38ab49dee40aef881d96c4e0b7e607e8
Zlotnik-Ilya <ilya.zlotnik@mail.ru>
2022-12-02 22:24:21 +0300

    feat:  add hw12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,26 @@
+// import React from 'react';
+
 const initState = {
     themeId: 1,
 }
+export type themeReducerType = {
+    themeId: number
+}
+export type changeThemeIdtActionType = {
+    type: 'SET_THEME_ID',
+    id: number
+}
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: changeThemeIdtActionType): themeReducerType => { // fix any
     switch (action.type) {
-        // дописать
+        case 'SET_THEME_ID' :{
+            return {...state, themeId: action.id}
+        }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+
+export const changeThemeId = (id: number): changeThemeIdtActionType => ({ type: 'SET_THEME_ID', id }) // fix any
