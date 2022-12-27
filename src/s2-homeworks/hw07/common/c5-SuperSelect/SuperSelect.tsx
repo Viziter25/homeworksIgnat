358be31f0e2052c7bb1b3b8ccb7f72025bb4468d
Zlotnik-Ilya <ilya.zlotnik@mail.ru>
2022-12-27 15:16:22 +0300

    feat:  add  hw15

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -36,8 +36,9 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
         : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        onChangeOption?.(Number(e.currentTarget.value))
-        console.log(e.currentTarget.value)
+        onChange?.(e)
+        onChangeOption && onChangeOption(+e.currentTarget.value)
+        // console.log(e.currentTarget.value)
     }
 
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
